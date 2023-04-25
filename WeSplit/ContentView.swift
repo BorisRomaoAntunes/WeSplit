d2a3f5fdd7d77d29839da02e8c3ceda68a76a7e1
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-04-25 20:03:20 -0300

    Cirando app para dividir a conta e calcular gorjeta

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -6,21 +6,28 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section{
+                TextField("Amount", value: $checkAmount, format: .currency(code: "BRL"))
+                // .currency(code: Locale.current.currencyCode ?? "AUD"))precisao aprender como verificar a localização e a moenda que ela usa como padrão. por hora irei colocar um tipo padrão
+                    .keyboardType(.decimalPad)
+            }
+            Section{
+                Text(checkAmount, format:  .currency(code: "BRL"))
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
