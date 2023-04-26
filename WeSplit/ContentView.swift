2f88cc8bebeda34d72a389313c31aeacb197f97c
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-04-26 17:46:13 -0300

    Aprendi a adicionar botão de voltar no teclado com a barra de ferramentas, o app agora ela fazendo o calculo  e tem uma  navegationView

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,22 +9,67 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = [10, 15, 20, 25, 0]
+    @State private var tipPercentage = 0
+    
+    @FocusState private var amoutIsFocused: Bool
+    
+    var tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerson: Double{
+        let peopleCout = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCout
+        
+        return amountPerPerson
+    }
     
     var body: some View {
-        Form {
-            Section{
-                TextField("Amount", value: $checkAmount, format: .currency(code: "BRL"))
-                // .currency(code: Locale.current.currencyCode ?? "AUD"))precisao aprender como verificar a localização e a moenda que ela usa como padrão. por hora irei colocar um tipo padrão
-                    .keyboardType(.decimalPad)
+        NavigationView{
+            Form {
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: "BRL"))
+                        .keyboardType(.decimalPad)
+                        .focused($amoutIsFocused)
+                    
+                    Picker ("Number of people", selection: $numberOfPeople){
+                        ForEach (2..<100){
+                            Text("\($0) pessoas")
+                        }
+                    }
+                }
+                Section{
+                    Picker("Tip Percetage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text ("How much tip do you want to leave")
+                }
+                
+                Section{
+                    Text(totalPerson, format:  .currency(code: "BRL"))
+                        .font(.largeTitle)
+                } header: {
+                    Text("total payable")
+                }
             }
-            Section{
-                Text(checkAmount, format:  .currency(code: "BRL"))
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    
+                    Button ("Done"){
+                        amoutIsFocused = false
+                    }
+                }
             }
         }
     }
-    
-    
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
